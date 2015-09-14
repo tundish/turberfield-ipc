1eed462185fc1287dea31a175ceb7d9d7d8e445f
Tundish <tundish@thuswise.org>
2015-09-14 18:55:14 +0100

    First structure.

---
+++ README.txt
@@ -0,0 +1,79 @@
+..  Titling
+    ##++::==~~--''``
+
+Turberfield is a framework for discrete event simulations and turn-based
+games.
+
+It is developed by a private individual but released to the public under the
+`GNU General Public License`_.
+
+The API may change as the project proceeds. At this early stage you should
+only use the latest release; it may not be compatible with previous
+versions.
+
+Requirements
+::::::::::::
+
+* Turberfield runs on Python 3.4 or later.
+* Some modules require atomic file renaming and named pipes (ie: a POSIX OS).
+
+Usage
+:::::
+
+Download and unpack the source distribution::
+
+    $ tar -xzvf turberfield-ipc-0.001.0.tar.gz
+    $ cd turberfield-ipc-0.001.0
+
+Run the tests::
+
+    $ python3.4 -m unittest discover turberfield
+
+Consult the documentation::
+
+    $ firefox turberfield/ipc/doc/html/index.html
+
+Installation
+::::::::::::
+
+::
+
+    $ pip install turberfield-ipc
+
+Roadmap
+:::::::
+
+Turberfield is a *S* ur *PRISE*.
+
+**Semantic**
+    Turberfield can be understood by machines or human beings.
+
+**Persistent**
+    Turberfield can be stopped and saved for later.
+
+**Reusable**
+    Turberfield can be turned into something else.
+
+**Interactive**
+    Turberfield listens to what you say.
+
+**Simulation**
+    Turberfield knows what it's talking about.
+
+for **Economics**
+    ...or Education, or Entertainment. Turberfield is deadly serious.
+    And only a game.
+
+What's missing? *UR*!
+
+* If you've spotted a bug in Turberfield, please let me know so I can fix it.
+* If you think Turberfield lacks a feature, you can help drive development by describing
+  your Use Case.
+
+In either event, please leave a message on the project's `message board`_.
+
+:Author: tundish
+:Copyright: 2015 D Haynes
+
+.. _GNU General Public License: http://www.gnu.org/licenses/gpl.html
+.. _message board: https://www.assembla.com/spaces/turberfield/messages
---
+++ setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+
+import ast
+import os.path
+
+from setuptools import setup
+
+
+try:
+    # Includes bzr revision number
+    from turberfield.ipc.about import version
+except ImportError:
+    try:
+        # For setup.py install
+        from turberfield.ipc import __version__ as version
+    except ImportError:
+        # For pip installations
+        version = str(ast.literal_eval(
+                    open(os.path.join(os.path.dirname(__file__),
+                    "turberfield", "ipc", "__init__.py"),
+                    'r').read().split("=")[-1].strip()))
+
+__doc__ = open(os.path.join(os.path.dirname(__file__), "README.txt"),
+               'r').read()
+
+setup(
+    name="turberfield-ipc",
+    version=version,
+    description="A Distributed Inter-Process Control Facility from the Turberfield project.",
+    author="D Haynes",
+    author_email="tundish@thuswise.org",
+    url="https://www.assembla.com/spaces/turberfield/messages",
+    long_description=__doc__,
+    classifiers=[
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.4",
+        "License :: OSI Approved :: GNU General Public License v3"
+        " or later (GPLv3+)"
+    ],
+    namespace_packages=["turberfield"],
+    packages=[
+        "turberfield.ipc",
+        "turberfield.ipc.test",
+    ],
+    package_data={
+        "turberfield.ipc": [
+            "doc/*.rst",
+            "doc/_templates/*.css",
+            "doc/html/*.html",
+            "doc/html/*.js",
+            "doc/html/_sources/*",
+            "doc/html/_static/css/*",
+            "doc/html/_static/font/*",
+            "doc/html/_static/js/*",
+            "doc/html/_static/*.css",
+            "doc/html/_static/*.gif",
+            "doc/html/_static/*.js",
+            "doc/html/_static/*.png",
+            ],
+    },
+    install_requires=[],
+    tests_require=[],
+    entry_points={
+        "console_scripts": [],
+    },
+    zip_safe=False
+)
