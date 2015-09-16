ed2d96d073d317eeb93712af0a9bbd5f496f1e8b
tundish <tundish@thuswise.org>
2015-09-16 18:12:04 +0100

    First play with flow creation passes test.

--- turberfield/ipc/test/test_records.py
+++ turberfield/ipc/test/test_records.py
@@ -18,8 +18,11 @@
 
 
 from collections import namedtuple
+import getpass
 import json
+import os.path
 import pkg_resources
+import tempfile
 
 import unittest
 
@@ -39,21 +42,45 @@ Path = namedtuple(
     ["root", "namespace", "user", "service", "application", "flow", "policy", "suffix"]
 )
 
+def new_flow(path:Path, prefix="flow_", suffix=""):
+    if all(path[:4]) and not any(path[5:]):
+            parent = os.path.join(*path[:5])
+            os.makedirs(parent, exist_ok=True)
+            flow = tempfile.mkdtemp(suffix=suffix, prefix=prefix, dir=parent)
+            return path._replace(flow=os.path.basename(flow))
+    else:
+        return path
+
+def recent_slot(path:Path):
+    slots = [i for i in os.listdir(os.path.join(path.root, path.home))
+             if os.path.isdir(os.path.join(path.root, path.home, i))]
+    stats = [(os.path.getmtime(os.path.join(path.root, path.home, fP)), fP)
+             for fP in slots]
+    stats.sort(key=operator.itemgetter(0), reverse=True)
+    return Persistent.Path(
+        path.root, path.home, next((i[1] for i in stats), None), path.file)
+
 class RecordTests(unittest.TestCase):
+
+    def setUp(self):
+        self.root = tempfile.TemporaryDirectory()
+
+    def tearDown(self):
+        if os.path.isdir(self.root.name):
+            self.root.cleanup()
+        self.assertFalse(os.path.isdir(self.root.name))
+        self.root = None
 
     def test_path(self):
         udp = turberfield.ipc.policy.POA.UDP(654)
         tx = turberfield.ipc.policy.Role.TX(500, 50, 50)
         record = json.dumps(vars(tx), indent=0, ensure_ascii=False, sort_keys=False)
-        self.fail(
+        rv = new_flow(
             Path(
-                ".turberfield",
-                "addisonarches",
-                "tundish",
-                "demo",
-                "addisonarches-web",
-                "flow00839",
-                "tx",
-                ".json"
+                ".turberfield", "addisonarches", getpass.getuser(),
+                "test", "addisonarches-web",
+                None, None, None
             )
         )
+        self.assertTrue(rv.flow)
+        self.assertTrue(os.path.isdir(os.path.join(*rv[0:5])))
