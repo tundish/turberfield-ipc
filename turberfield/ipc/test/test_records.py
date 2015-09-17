8c9ac14919c0d4f2a97d9268e8447bc104e84f53
tundish <tundish@thuswise.org>
2015-09-17 09:46:43 +0100

    Sketching out flow API.

--- turberfield/ipc/test/test_records.py
+++ turberfield/ipc/test/test_records.py
@@ -37,30 +37,72 @@ def package_interface(key="turberfield.ipc.role"):
         else:
             yield (i.name, ep)
 
-Path = namedtuple(
-    "Path",
+Resource = namedtuple(
+    "Resource",
     ["root", "namespace", "user", "service", "application", "flow", "policy", "suffix"]
 )
 
-def new_flow(path:Path, prefix="flow_", suffix=""):
-    if all(path[:4]) and not any(path[5:]):
-            parent = os.path.join(*path[:5])
-            os.makedirs(parent, exist_ok=True)
-            flow = tempfile.mkdtemp(suffix=suffix, prefix=prefix, dir=parent)
-            return path._replace(flow=os.path.basename(flow))
-    else:
-        return path
+class Flow:
 
-def recent_slot(path:Path):
+    @staticmethod
+    def create(path:Resource, prefix="flow_", suffix=""):
+        if all(path[:4]) and not any(path[5:]):
+                parent = os.path.join(*path[:5])
+                os.makedirs(parent, exist_ok=True)
+                flow = tempfile.mkdtemp(suffix=suffix, prefix=prefix, dir=parent)
+                return path._replace(flow=os.path.basename(flow))
+        else:
+            return path
+
+    @staticmethod
+    def invite(flow:Resource, application:Resource):
+        pass
+
+    @staticmethod
+    def get_invites(application:Resource):
+        """
+        Return all open invites.
+        """
+        pass
+
+    @staticmethod
+    def get_applications(flow:Resource):
+        """
+        Return all applications party to a flow.
+        """
+        pass
+
+    @staticmethod
+    def get_flows(application:Resource):
+        """
+        Return all flows associated with the application.
+        """
+        pass
+
+    @staticmethod
+    def accept(invite:Resource):
+        """
+        Accepts an invite.
+        """
+        pass
+
+    @staticmethod
+    def refuse(invite:Resource):
+        """
+        Refuses an invite.
+        """
+        pass
+
+def recent_slot(path:Resource):
     slots = [i for i in os.listdir(os.path.join(path.root, path.home))
              if os.path.isdir(os.path.join(path.root, path.home, i))]
     stats = [(os.path.getmtime(os.path.join(path.root, path.home, fP)), fP)
              for fP in slots]
     stats.sort(key=operator.itemgetter(0), reverse=True)
-    return Persistent.Path(
+    return Persistent.Resource(
         path.root, path.home, next((i[1] for i in stats), None), path.file)
 
-class RecordTests(unittest.TestCase):
+class FlowTests(unittest.TestCase):
 
     def setUp(self):
         self.root = tempfile.TemporaryDirectory()
@@ -71,12 +113,12 @@ class RecordTests(unittest.TestCase):
         self.assertFalse(os.path.isdir(self.root.name))
         self.root = None
 
-    def test_path(self):
+    def test_create(self):
         udp = turberfield.ipc.policy.POA.UDP(654)
         tx = turberfield.ipc.policy.Role.TX(500, 50, 50)
         record = json.dumps(vars(tx), indent=0, ensure_ascii=False, sort_keys=False)
-        rv = new_flow(
-            Path(
+        rv = Flow.create(
+            Resource(
                 ".turberfield", "addisonarches", getpass.getuser(),
                 "test", "addisonarches-web",
                 None, None, None
@@ -84,3 +126,21 @@ class RecordTests(unittest.TestCase):
         )
         self.assertTrue(rv.flow)
         self.assertTrue(os.path.isdir(os.path.join(*rv[0:5])))
+
+    def test_attach(self):
+        udp = turberfield.ipc.policy.POA.UDP(654)
+        tx = turberfield.ipc.policy.Role.TX(500, 50, 50)
+        record = json.dumps(vars(tx), indent=0, ensure_ascii=False, sort_keys=False)
+        master = Resource(
+            ".turberfield", "turberfield", getpass.getuser(),
+            "test", "turberfield-master",
+            None, None, None
+        )
+        slave = Resource(
+            ".turberfield", "turberfield", getpass.getuser(),
+            "test", "turberfield-slave",
+            None, None, None
+        )
+        flow = Flow.create(master)
+        rv = Flow.invite(flow, slave)
+        self.assertTrue(rv)
