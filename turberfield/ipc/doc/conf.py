d29b6cb16f58082aeaebd10023f0b839632b08ea
tundish <tundish@thuswise.org>
2015-10-07 10:35:54 +0100

    Docs rendered with 'haiku' stylesheet now.

--- turberfield/ipc/doc/conf.py
+++ turberfield/ipc/doc/conf.py
@@ -137,7 +137,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "haiku"
+#html_theme = "haiku"
 
 # More elaborate themes are commented out
 # on_rtd is whether we are on readthedocs.org
@@ -149,6 +149,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+html_theme = "haiku"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
