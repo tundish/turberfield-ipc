99cf5c620b1fc939aa5094e7388b11b605fc529e
Tundish <tundish@thuswise.org>
2016-02-02 21:12:42 +0000

    Updated pinning.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     },
     install_requires=[
         "rson>=0.9",
-        "turberfield-utils>=0.20.0",
+        "turberfield-utils>=0.21.0",
     ],
     extras_require={
         "docbuild": [
